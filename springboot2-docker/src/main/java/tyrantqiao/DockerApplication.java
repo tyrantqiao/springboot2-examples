1f186ef4281c0127ee384c5195f8d41d94b8087a
Administrator <tyrantqiao@icloud.com>
2018-05-07 22:12:15 +0800

    spring-boot docker

---
+++ springboot2-docker/src/main/java/tyrantqiao/DockerApplication.java
@@ -0,0 +1,15 @@
+package tyrantqiao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author tyrantqiao [tyrantqiao@gmail.com]
+ * @see <a href="https://github.com/tyrantqiao">qiao's github</a>
+ */
+@SpringBootApplication
+public class DockerApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(DockerApplication.class, args);
+	}
+}
---
+++ springboot2-docker/src/main/java/tyrantqiao/controller/DockerController.java
@@ -0,0 +1,16 @@
+package tyrantqiao.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author tyrantqiao [tyrantqiao@gmail.com]
+ * @see <a href="https://github.com/tyrantqiao">qiao's github</a>
+ */
+@RestController
+public class DockerController {
+	@GetMapping("/")
+	public String hello() {
+		return "hello docker";
+	}
+}
